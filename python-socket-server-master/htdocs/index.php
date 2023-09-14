9af85ca75d1727b4486a36e6998c44ce7ce0b527
Tuan Siraj <136558994+Siraj311@users.noreply.github.com>
2023-09-14 14:20:22 +0530

    Add files via upload

---
+++ python-socket-server-master/htdocs/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="./sum.php" method="POST">
+    <input type="text" name ="a">
+    <input type="text" name ="b">
+
+  <button>Submit</button>
+  </form>
+  
+  
+</body>
+</html>
+
